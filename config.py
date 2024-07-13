920148186eac1cd4b2c6f8f96d0407dd74e68c73
Ayanokozii <ayanokozi0925@gmail.com>
2024-07-13 17:39:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,21 +6,21 @@ id_pattern = re.compile(r'^.\d+$')
 class Config(object):
 
     # Client Config 
-    API_ID = int(os.environ.get('API_ID', '')) # ⚠️ Required
-    API_HASH = os.environ.get('API_HASH', '') # ⚠️ Required
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '') # ⚠️ Required
+    API_ID = int(os.environ.get('API_ID', '21189715')) # ⚠️ Required
+    API_HASH = os.environ.get('API_HASH', '988a9111105fd2f0c5e21c2c2449edfd') # ⚠️ Required
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '7296474165:AAHypE_ZatCtoRqd2UFSaeqSvz-d96TZEmk') # ⚠️ Required
 
     # database config
-    DB_URL  = os.environ.get("DB_URL","") # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://ayanosuvii0925:subhichiku123@cluster0.uw8yxkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # ⚠️ Required
 
     # Other Config 
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # ⚠️ Required
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "https://t.me/sifrapprovalbot") # ⚠️ Required
     BOT_UPTIME  = time.time()
-    OWNER = int(os.environ.get('OWNER', '')) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # ⚠️ Required
+    OWNER = int(os.environ.get('OWNER', '6798912985')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002149291828")) # ⚠️ Required
     APPROVED_WELCOME_TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYou're Auto Approved. ✅")
     LEAVING_BY_TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "👋 Bye {mention} !\nSee You Soon by {title}\n\nYou Left. ⛔")
-    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', '1002149291828') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None 
 
